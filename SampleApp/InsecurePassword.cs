e3ec72368f1ec94dab6a43768acb922cf2a3b905
IrfanSyed-PSC <isyed@progress.com>
2020-11-27 00:19:08 -0500

    fix bad code

--- SampleApp/InsecurePassword.cs
+++ SampleApp/InsecurePassword.cs
@@ -4,30 +4,4 @@ using System;
 public class InsecurePassword
 {
 
-	string GeneratePassword()
-	{
-		// BAD: Password is generated using a cryptographically insecure RNG
-		Random gen = new Random();
-		string password = "mypassword" + gen.Next();
-
-		return password;
-	}
-
-	public void badpassword()
-	{
-		Random gen = new Random();
-		string password = "password" + gen.Next();
-	}
-
-	public string badpasswordcomparison()
-	{
-		string password = "password";
-		//bad comparison
-		if (password == "password")
-		{
-			return password;
-		}
-
-		return password;
-	}
 }
